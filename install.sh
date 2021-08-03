aca41fcd57f9f831f808a82a635c847ed0c79678
Sven Goericke <sven.goericke@posteo.de>
2021-08-03 14:01:32 +0200

    new commit

--- install.sh
+++ install.sh
@@ -7,16 +7,16 @@ CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)
 read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
 sudo hostnamectl set-hostname --pretty "${PRETTY_HOSTNAME:-${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}}"
 
-echo "Updating packages"
-sudo apt update
-sudo apt upgrade -y
+#echo "Updating packages"
+#sudo apt update
+#sudo apt upgrade -y
 
 echo "Installing components"
 sudo ./install-bluetooth.sh
-sudo ./install-shairport.sh
-sudo ./install-spotify.sh
-sudo ./install-upnp.sh
-sudo ./install-snapcast-client.sh
-sudo ./install-pivumeter.sh
-sudo ./enable-hifiberry.sh
-sudo ./enable-read-only.sh
+#sudo ./install-shairport.sh
+#sudo ./install-spotify.sh
+#sudo ./install-upnp.sh
+#sudo ./install-snapcast-client.sh
+#sudo ./install-pivumeter.sh
+#sudo ./enable-hifiberry.sh
+#sudo ./enable-read-only.sh
